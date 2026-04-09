42f0f624ff474bed5091174a75c7f2d93dfd9c43
ruchirajkarki <ruchirajkarki@gmail.com>
2026-04-09 21:25:59 +0545

    feat: replace .prettierrc with prettier.config.ts

---
+++ prettier.config.ts
@@ -0,0 +1,12 @@
+// prettier.config.ts, .prettierrc.ts, prettier.config.mts, or .prettierrc.mts
+
+import type { Config } from 'prettier';
+
+const config: Config = {
+    trailingComma: 'none',
+    singleQuote: true,
+    printWidth: 80,
+    tabWidth: 4
+};
+
+export default config;
