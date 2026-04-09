e41f6a19a2fa604e291ec66979d636f259d05d79
Ruchi Raj Karki <ruchirajkarki@gmail.com>
2026-04-09 21:31:37 +0545

    feat/prettier formatter (#2)
    * feat: add Prettier configuration and dependencies
    
    * feat: replace .prettierrc with prettier.config.ts
    
    * fix: update prettier to remove semi colons
    
    * chore: run prettier format
    
    * feat: add format script to package.json for Prettier

---
+++ prettier.config.ts
@@ -0,0 +1,13 @@
+// prettier.config.ts, .prettierrc.ts, prettier.config.mts, or .prettierrc.mts
+
+import type { Config } from 'prettier'
+
+const config: Config = {
+    trailingComma: 'none',
+    singleQuote: true,
+    printWidth: 80,
+    tabWidth: 4,
+    semi: false
+}
+
+export default config
