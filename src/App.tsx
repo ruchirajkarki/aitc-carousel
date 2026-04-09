0f4340569f3a05dfe117405d3b5fe5c89bd8543a
ruchirajkarki <ruchirajkarki@gmail.com>
2026-04-09 23:19:25 +0545

    feat: implement Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
+import { Header } from './components/header'
+
 function App() {
     return (
         <>
-            <div >
-                <h1 className='text-5xl font-bold'>Tailwind test</h1>
-            </div>
-            <div>
-                <p className='text-2xl text-gray-500'>Tailwind is working Properly</p>
-            </div>
+            <Header className='mt-20'/>
         </>
     )
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,52 @@
+import type { ComponentProps } from 'react'
+
+interface HeaderProps extends ComponentProps<'header'> {
+    /**
+     * Heading text to be displayed in the header. Optional.
+     * @default "Simple Image Carousel"
+     */
+    heading?: string
+
+    /**
+     * Subheading text to be displayed below the main heading. Optional.
+     * @default predesigned from figma:
+     * https://www.figma.com/design/cRU4R1JpyFW29kClgedf78/Carousel--Community-?node-id=3-100&t=WwWjw1BbyzsPjYhx-4
+     */
+    subHeading?: string
+}
+
+function Subheading({ subHeading }: { readonly subHeading?: string }) {
+
+    if(subHeading){
+        return (
+            <p className="text-xl text-gray-500">
+                {subHeading}
+            </p>
+        )
+    }
+
+    return (
+      <div className='text-center'>
+        <p>
+           5 x Image items <span className="font-bold">❖ Carousel</span>
+        </p>
+        <p>
+            Interactive Components & Variants
+        </p>
+      </div>
+    )
+}
+
+export const Header: React.FC<HeaderProps> = ({
+    heading = 'Simple Image Carousel',
+    subHeading,
+    ...props
+}) => {
+
+    return (
+        <header {...props} className="text-center mb-8 flex flex-col gap-6">
+            {heading && <h1 className="text-6xl font-bold">{heading}</h1>}
+            <Subheading subHeading={subHeading} />
+        </header>
+    )
+}
