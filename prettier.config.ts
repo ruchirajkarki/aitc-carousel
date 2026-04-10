3abccd119e1e15794c603b6318c26cd3bfa452b9
ruchirajkarki <ruchirajkarki@gmail.com>
2026-04-10 07:16:49 +0545

    feat: sort import by prettier

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,13 +1,16 @@
 // prettier.config.ts, .prettierrc.ts, prettier.config.mts, or .prettierrc.mts
-
 import type { Config } from 'prettier'
 
 const config: Config = {
+    plugins: ['@trivago/prettier-plugin-sort-imports'],
     trailingComma: 'none',
     singleQuote: true,
     printWidth: 80,
     tabWidth: 4,
-    semi: false
+    semi: false,
+    importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true
 }
 
 export default config
